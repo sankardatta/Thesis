e80b4124914f0f63af9bdfbcc9599b5826f554d8
sankar <sankarnarayandatta@gmail.com>
2015-03-11 12:48:10 +0100

    changed image file

--- Sonic1/Sonic1/main.cpp
+++ Sonic1/Sonic1/main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 void main()
 {
     //Mat im = imread("C:/Users/Sankar/Desktop/lena.jpg");
-	imReadBasics ob = imReadBasics("C:/Users/Sankar/Desktop/lena.jpg");
+	imReadBasics ob = imReadBasics("C:/Users/Sankar/Desktop/product.png");
 	if (!ob.im.data) 
     {
         cout << "Cannot load image!" << endl;
