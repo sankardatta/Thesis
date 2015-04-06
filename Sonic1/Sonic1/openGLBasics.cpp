9827db6c2907abb7d9e04325f77c4c09d1adeb7c
sankar <sankardatt@gmail.com>
2015-04-06 22:09:43 +0200

    Fixed openGL initial bug

--- Sonic1/Sonic1/openGLBasics.cpp
+++ Sonic1/Sonic1/openGLBasics.cpp
@@ -1,15 +1,15 @@
 #define GLEW_STATIC
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
 #include <iostream>
 #include <thread>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include "openGLBasics.h"
 
 using namespace std;
 
 openGLBasics::openGLBasics(void)
 {
-    cout<<"Inside GLBasics"<<endl;
+    cout<<"Inside GLBasics."<< endl;
 }
 
 
@@ -19,17 +19,59 @@ openGLBasics::~openGLBasics(void)
 
 void openGLBasics::mainGL()
 {
-	glfwInit();
-	
-	//glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	//glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwInit();
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", nullptr, nullptr);
+    glfwMakeContextCurrent(window); 
+    //GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", glfwGetPrimaryMonitor(), nullptr); //Fullsreen
 
-	//glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glewExperimental = GL_TRUE;
+    GLenum err = glewInit();
+    
+    if(err!=GLEW_OK)
+    {
+        std::cout << "glewInit failed, aborting. Code " << err << ". " << std::endl;
+    }
+    
+    float vertices[] = { 0.0f,  0.5f, // Vertex 1 (X, Y)
+                        0.5f, -0.5f, 
+                        -0.5f, -0.5f  
+                        };
+    GLuint vbo;
+    
+    try 
+    {
+        cout << "Generating VBO"<< endl;
+        glGenBuffers(1, &vbo);
+        cout << "Generated VBO: "<< vbo<< endl;
+    }
+    catch(exception& e)
+    {
+        cout<< e.what()<< endl;
+    }
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", nullptr, nullptr); // Windowed
-	//GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", glfwGetPrimaryMonitor(), nullptr); //Fullsreen
+    try 
+    {
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    }
+    catch(exception& e)
+    {
+        cout<<e.what()<<endl;
+    }
+    
+    /*
+    glfwMakeContextCurrent(window);
+    
+    while(!glfwWindowShouldClose(window))
+    {
+        glBegin(GL_LINES);
+        glVertex2f(10.0, 10.0);
+        glVertex2f(50.0, 50.0);
+        glEnd();
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    */
     std::this_thread::sleep_for(std::chrono::seconds(5));
 	glfwTerminate();
 
